ef44b1ba132f04d72c763ba089bd9d5175ad8067
Peter BUSS <peter.buss@hispeed.ch>
2015-08-01 19:52:09 +0000

    Create style and ingredients display and ui

---
+++ app/controllers/styles_controller.rb
@@ -0,0 +1,31 @@
+class StylesController < ApplicationController
+  
+  before_action :require_user, except: [:show]  
+  
+  def show
+    @style = Style.find(params[:id])  
+    @recipes = @style.recipes.paginate(page: params[:page], per_page: 4)
+  end
+  
+  def new
+    # need instance var
+    @style = Style.new
+  end
+  
+  def create
+    @style = Style.new(style_params)
+    if @style.save
+      flash[:success] = "Style was created sucessfully"
+      redirect_to recipes_path
+    else
+      render 'new'
+    end
+  end
+  
+  private
+  
+  def style_params
+    params.require(:style).permit(:name)
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,9 @@ Rails.application.routes.draw do
   post 'login', to: "logins#create"
   get 'logout', to: "logins#destroy"
   
+  resources :styles, only: [:new, :create, :show]
+  resources :ingredients, only: [:new, :create, :show]
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
